03476feb02afd900c2fe7a849f9bad49a8e951d4
dave motari <davemotari22@gmail.com>
2022-09-20 00:14:35 +0300

    Task 4 correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 # set up  client SSH configuration file so that connection to the server is without typing a password.
 
-file_line{
+file_line { 'Remove password auth':
   path    =>  '/etc/ssh/ssh_config',
   line    =>  '    PasswordAuthentication no',
-  replace =>  'true',
+  replace =>  true,
 }
-file_line{
+
+file_line { 'Change identity file':
   path    =>  '/etc/ssh/ssh_config',
   line    =>  '    IdentityFile ~/.ssh/school',
-  replace =>  'true',
+  replace =>  true,
 }
 
