8a847265eb9fada86c1477840274b10b409749d6
David Motari <58317281+David-Motari@users.noreply.github.com>
2022-11-17 17:05:55 +0300

    Delete web_stack_debugging_3 directory

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# fix of typo of PHP in wp-settings.php file
-exec { 'fix-wp-error-log':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-}
