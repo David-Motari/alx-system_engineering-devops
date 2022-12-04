7bc0f8cf31b069d9b949f1e98e4116bd3270240d
David_Motari <davemotari22@gmail.com>
2022-12-04 07:45:45 +0300

    Task 3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API, parses the title of all hot articles, 
+and prints a sorted count of given keywords (case-insensitive,
+delimited by spaces.
+"""
+
+import requests
+
+
+url = 'http://reddit.com/r/{}/hot.json'.format(subreddit)
+
+
+def count_words(subreddit, word_list, hot_list=[], after=""):
+    """
+    all posts recursively
+    """
+    headers = {'User-agent': 'karenahv'}
+    params = {'t': all, 'after': after}
+    req = requests.get(url, headers=headers,
+                       params=params)
+    if not req or req.status_code != 200:
+        return None
+    info = req.json()
+    for hot in info['data']['children']:
+        hot_list.append(hot['data']['title'])
+    after = info.get('data').get('after')
+    if after:
+        count_words(subreddit, word_list, hot_list, after)
+    return hot_list
