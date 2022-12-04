8e72fd0dffc5312afd1a770f3c53ff3ba5855197
David_Motari <davemotari22@gmail.com>
2022-12-04 07:47:06 +0300

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-queries the Reddit API, parses the title of all hot articles, 
+queries the Reddit API, parses the title of all hot articles,
 and prints a sorted count of given keywords (case-insensitive,
 delimited by spaces.
 """
